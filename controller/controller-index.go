329bcdd57130d783124f55e0f9608433e0bf8ede
lovebai <1507820372@qq.com>
2024-08-15 22:10:54 +0800

    前台页面显示处理完毕

---
+++ controller/controller-index.go
@@ -0,0 +1,51 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index/index", gin.H{
+		"version": "1.0.0",
+		"title":   "首页",
+	})
+	fmt.Println("访问首页")
+}
+
+func Little(c *gin.Context) {
+	c.HTML(http.StatusOK, "index/little", gin.H{
+		"title": "点点滴滴",
+	})
+
+}
+
+func Leaving(c *gin.Context) {
+	c.HTML(http.StatusOK, "index/leaving", gin.H{
+		"title": "留言",
+	})
+
+}
+
+func Photo(c *gin.Context) {
+	c.HTML(http.StatusOK, "index/photo", gin.H{
+		"title": "相册",
+	})
+
+}
+
+func TodoList(c *gin.Context) {
+	c.HTML(http.StatusOK, "index/todolist", gin.H{
+		"title": "恋爱列表",
+	})
+
+}
+
+func About(c *gin.Context) {
+	c.HTML(http.StatusOK, "index/about", gin.H{
+		"title": "关于",
+	})
+
+}
--- router/router.go
+++ router/router.go
@@ -7,18 +7,29 @@ import (
 )
 
 func Start() {
-	e := gin.Default()
-	e.LoadHTMLGlob("templates/*")
-	e.Static("/static", "./static")
+	router := gin.Default()
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+	router.LoadHTMLGlob("templates/**/*")
+	router.Static("/static", "./static")
 
 	//前台路由
-	e.GET("/", controller.Index)
-	e.GET("/Little", controller.Little)
-	e.GET("/Lenving", controller.Leaving)
-	e.GET("/Photo", controller.Photo)
-	e.GET("/TodoList", controller.TodoList)
-	e.GET("/About", controller.About)
+	index := router.Group("/")
+	{
+		index.GET("/", controller.Index)
+		index.GET("/Little", controller.Little)
+		index.GET("/Lenving", controller.Leaving)
+		index.GET("/Photo", controller.Photo)
+		index.GET("/TodoList", controller.TodoList)
+		index.GET("/About", controller.About)
+	}
 
-	e.Run(":8100")
+	//后台路由
+	admin := router.Group("/Admin")
+	{
+		admin.GET("/", func(ctx *gin.Context) { ctx.JSON(200, gin.H{"name": "admin", "site": "obai.cc"}) })
+		admin.GET("/list", nil)
+	}
+
+	router.Run(":8100")
 
 }
