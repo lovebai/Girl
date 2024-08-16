f7012ac036334cda3b56d43346b56ec324509176
lovebai <1507820372@qq.com>
2024-08-16 21:22:21 +0800

    add file

--- controller/admin/admin.go
+++ controller/admin/admin.go
@@ -1,1 +1,13 @@
 package adminController
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Login(c *gin.Context) {
+	c.HTML(http.StatusOK, "admin/login", gin.H{
+		"title": "管理员登录页面",
+	})
+}
--- router/admin.go
+++ router/admin.go
@@ -1,11 +1,15 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	adminController "Girl/controller/admin"
+
+	"github.com/gin-gonic/gin"
+)
 
 func AdminRouter(router *gin.Engine) {
 	admin := router.Group("/Admin")
 	{
 		admin.GET("/", func(ctx *gin.Context) { ctx.JSON(200, gin.H{"name": "admin", "site": "obai.cc"}) })
-		admin.GET("/list", nil)
+		admin.GET("/login", adminController.Login)
 	}
 }
